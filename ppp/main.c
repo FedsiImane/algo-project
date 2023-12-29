e8319c7541e2e9d5ee1c2e3b45b69faac98afc14
Imane Fedsi <iman14092003@gmail.com>
2023-12-29 20:07:27 +0100

    Initial Commit

---
+++ ppp/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ppp
+//
+//  Created by imannina on 29/12/2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
